79c987a0cec2ab32fe2ee88599dc0cb01c8d548b
tszhong0411 <lainelson411@gmail.com>
2021-03-07 23:47:09 +0800

    Update

--- moments.js
+++ moments.js
@@ -183,3 +183,6 @@ fetch(requests_url).then(
         data_handle(orign_data, maxnumber)
     }
 )
+if (document.getElementById("info_user_pool")) {
+    document.getElementById("loaderOfFcircle").style.display = "none";
+}
